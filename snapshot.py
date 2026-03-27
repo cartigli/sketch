e6ced2c0db19953f4f8e3bd7b29120f64571393b
cartigli <tommy@cartiglia.co>
2026-03-26 21:50:14 -0500

    opencv cam calibration + applied reprojection correction

---
+++ snapshot.py
@@ -0,0 +1,62 @@
+import os
+import cv2
+import shutil
+from contextlib import contextmanager
+
+"""
+Streams a device's video feed and takes snapshots when the user presses 'x'.
+"""
+
+def out_mgr(out):
+     """Handles creation/deletion of the directory of calibration images."""
+     if os.path.exists(out):
+          rm = input(f"{out} alreaedy exists; replace it? [y/n]: ")
+          if rm in ("y", "Y", "yes", " y", "y "):
+               shutil.rmtree(out)
+          else:
+               print("understood; abandoning")
+               return False
+     os.mkdir(out)
+     return True
+
+
+@contextmanager
+def creek(src=0):
+     """Context manager for the video feed device & its closing."""
+     cap = cv2.VideoCapture(src, cv2.CAP_V4L2)
+     h, w = cap.read()[1].shape[:2]
+     print(f"loaded video feed w.dims: {w}x{h}")
+     yield cap
+     cap.release()
+     cv2.destroyAllWindows()
+
+
+def main(out, src=0):
+     """Plays a video feed while capturing frames when 'x' is pressed (q/e for quit/exit)."""
+     cnt = 0
+     if not out_mgr(out):
+          return
+
+     with creek(src) as cap:
+          while True:
+               r, f = cap.read()
+               if not r:
+                    print("failed to grab frame")
+                    break
+               cv2.imshow("Capturing Calibration:", f)
+
+               key = cv2.waitKey(1) & 0xFF
+               if key == ord("x"):
+                    nom = os.path.join(out, f"calimg_{cnt}.png")
+                    cv2.imwrite(nom, f)
+                    print(f"saved {nom} to disk")
+                    cnt += 1
+               elif key in (ord("q"), ord("e")):
+                    print("user quit")
+                    break
+
+
+if __name__=="__main__":
+     src = 2
+     out = "/home/t/cali.A1"
+     main(out, src)
--- vid_cali.py
+++ vid_cali.py
@@ -1,19 +1,54 @@
+import time
 import math
+import threading
 from contextlib import contextmanager
 
 import cv2
+import numpy as np
 
 
-def load_vid():
-     """Load a video feed from a cam into memory."""
-     cap = cv2.VideoCapture(0)
-     ret, f = cap.read()
-     if not ret:
-          print("Failed to grab frame.")
-          return None
-     h, w = f.shape[:2]
-     print(f"Loaded video feed w.dims: {w}x{h}")
-     return cap
+class Stream:
+     """Initiates video stream & starts thread to grab frames in the bg."""
+
+     def __init__(self, src=0, cald=None, bfsz=1):
+          """Initiates the video capture source, sets buffer size, initiates & locks thread, and unpacks reprojection data, if present."""
+          self.cap = cv2.VideoCapture(src, cv2.CAP_V4L2)
+          self.cap.set(cv2.CAP_PROP_BUFFERSIZE, bfsz)
+          self.ret, self.f = self.cap.read()
+
+          self._lock = threading.Lock()
+          self._stopped = False
+          threading.Thread(target=self._update, daemon=True).start()
+
+          if cald:
+               self.cal = np.load(cald)
+               self.map_x, self.map_y = self.cal["map_x"], self.cal["map_y"]
+          else:
+               self.cal = None
+
+     def _update(self):
+          """Threaded method for grabbing frames in the bg."""
+          while not self._stopped:
+               ret, f = self.cap.read()
+               with self._lock:
+                    self.ret, self.f = ret, f
+
+     def read(self):
+          """Grabs the most recent frame from threaded process.
+          Applies the calibration reprojection if present.
+          """
+          with self._lock:
+               if not self.ret:
+                    return None, None
+               if self.cal:
+                    fcal = cv2.remap(self.f.copy(), self.map_x, self.map_y, cv2.INTER_LINEAR)
+                    return self.ret, fcal
+               return self.ret, self.f.copy()
+
+     def kill(self):
+          """Stops the threaded process and releases the video capture generator."""
+          self._stopped = True
+          self.cap.release()
 
 
 def hypo(a, b):
@@ -24,7 +59,7 @@ def hypo(a, b):
 
 
 def demi(a, b):
-     """Finds the mdipoint of two coordinates."""
+     """Finds the midpoint of two coordinates."""
      x = (a[0] + b[0]) // 2
      y = (a[1] + b[1]) // 2
      return (x, y)
@@ -39,19 +74,24 @@ def verify(a, b):
      if tru <= 1.0 or lmk_dist <= 1.0:
           print("too small of a calibration region")
           return None
-     print(f"Calibrated for: {pix_per:.3f} pixels per mm ({pix_per:.3f}=1mm)")
 
+     print(f"Calibrated for: {pix_per:.3f} pixels per mm ({pix_per:.3f}=1mm)")
      return pix_per
 
 
-def artist(bg, x, y, text=None):
-     """Draws lines between coordinates pairs, with text if present."""
-     cv2.line(bg, x, y, (0, 0, 255), 2)
+def artist(bg, x, y, text=None, crosshairs=False):
+     """Draws lines between coordinates pairs, text if present, and crosshairs if applicable."""
+     cv2.line(bg, x, y, (0, 0, 255), 1)
      if text:
-          cv2.putText(bg, text, demi(x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+          cv2.putText(bg, text, demi(x, y), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 0, 0), 1)
+     if crosshairs:
+          size = 5
+          for e, f in (x, y):
+               cv2.line(bg, (e-size, f), (e+size, f), (0, 255, 0), 1)
+               cv2.line(bg, (e, f-size), (e, f+size), (0, 255, 0), 1)
 
 
-def watch_clicks(cap, win, pix_per, lines=None):
+def watch_clicks(stream, win, pix_per, lines=None, chain=False):
      """Watches the video feed & tracks mouse events; records left clicks' positions."""
      points = []
      live = None
@@ -63,36 +103,48 @@ def watch_clicks(cap, win, pix_per, lines=None):
                points.append((x, y))
           elif event == cv2.EVENT_RBUTTONDOWN:
                if points:
-                    points.clear() # erase last selection
+                    lines.pop()
                elif lines:
                     lines.pop() # clear the last created line
+               else:
+                    points.clear() # erase last selection
           elif event == cv2.EVENT_MOUSEMOVE:
                live = (x, y)
 
      cv2.setMouseCallback(win, mouse_event)
 
+     if lines and chain:
+          points.append(lines[-1][1])
+
      while len(points) < 2:
-          ret, f = cap.read()
+          ret, f = stream.read()
           if not ret:
-               print("failed to grab frame")
                return (None, None)
 
           if lines:
                for c, d, vrai in lines:
-                    artist(f, c, d, vrai)
+                    artist(f, c, d, vrai, crosshairs=True)
           if len(points) == 1 and live and pix_per:
-               artist(f, points[0], live, text=f"{(hypo(points[0], live)/pix_per):.3f} mm")
+               artist(f, points[0], live, text=f"{(hypo(points[0], live)/pix_per):.3f} mm", crosshairs=True)
           elif len(points) == 1 and live and not pix_per:
-               artist(f, points[0], live)
+               artist(f, points[0], live, crosshairs=True)
 
           cv2.imshow(win, f)
-
           key = cv2.waitKey(1)
+
           if key == ord('q') or key == 27:
                print("user quit")
                return (None, None)
+          elif key == ord('c'):
+               print(f"user changed chain from {chain} to {not chain}")
+               chain = not chain
+               if chain:
+                    if lines and not points:
+                         points.append(lines[-1][1])
+               if not chain:
+                    points.clear()
 
-     return points
+     return points, chain
 
 
 @contextmanager
@@ -104,38 +156,37 @@ def wm(win):
      cv2.destroyAllWindows()
 
 
-def main():
+def main(src=0, cald=None):
      pix_per = None
-     cap = load_vid()
-     if cap is None:
-          return
+     stream = Stream(src, cald)
 
      with wm("Select two points w.a known distance: ") as win:
-          a, b = watch_clicks(cap, win, pix_per)
-          cap.release() # close & reload so the window doesn't need to be closed/moved
-          if not a:
+          points, _ = watch_clicks(stream, win, pix_per)
+          stream.kill()
+          if not points:
                return
 
-     pix_per = verify(a, b)
+     pix_per = verify(*points)
      if not pix_per:
           return
 
-     cap = load_vid()
+     flood = Stream(src, cald)
      with wm("Choose two points to find their distance: ") as win:
+          chain = True
           lines = []
           while True:
-               c, d = watch_clicks(cap, win, pix_per, lines)
-               if not c:
+               points, chain = watch_clicks(flood, win, pix_per, lines, chain)
+               if not points:
                     break
 
-               eucl = hypo(c, d)
+               eucl = hypo(*points)
                vrai = f"{(eucl / pix_per):.3f} mm"
 
                print(f"true distance: {vrai}")
-               lines.append((c, d, vrai))
+               lines.append((*points, vrai))
 
-          cap.release()
+          flood.kill()
 
 
 if __name__=="__main__":
-     main()
+     main(src=2)
